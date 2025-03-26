04dbe1c41ea330fc294c1f61a46e6f7df94ad04e
LuKa44x <lucavicari44@gmail.com>
2025-03-26 16:11:52 +0100

    Added some functions for the create new grid btn
    Added a function for remove the old grid
    Transformed the create grid in a function to call every time i need
    fix a little style in the grid-item class.

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 let divGrid = [];
 
 const divContainer = document.querySelector(".gridContainer");
+gridCreation(256);
 
-
-for( let i=0;i<256;i++){
+function gridCreation(number){
+for( let i=0;i<number;i++){
     const div = document.createElement("div");
     div.classList.add('grid-item');
     divGrid.push(div);
+
+    divGrid.forEach(div => {
+        divContainer.appendChild(div);
+    })
+}
 }
 
-divGrid.forEach(div => {
-    divContainer.appendChild(div);
-})
+
+
+function gridRemotion(){
+    document.querySelectorAll(".grid-item").forEach(div => div.remove());
+    divGrid = [];
+}
+
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", ()=>{
+    let gridSize = parseInt(prompt("Number of square per side of the new grid?"));
+    gridRemotion();
+    gridCreation(gridSize);
+});
