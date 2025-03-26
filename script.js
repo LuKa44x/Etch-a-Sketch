f87d3f0840a38a6d36f77f15eeade7a1f0c2d947
LuKa44x <lucavicari44@gmail.com>
2025-03-26 17:42:15 +0100

    Added rgb background and lower bright on mouseover
    Added function for make the backgroundColor rgb with a random
    Little fix and moved some lines for a better work
    Remove in css the useless class hover.

--- script.js
+++ script.js
@@ -3,28 +3,55 @@ let divGrid = [];
 const divContainer = document.querySelector(".gridContainer");
 gridCreation(256);
 
-function gridCreation(number){
-for( let i=0;i<number;i++){
-    const div = document.createElement("div");
-    div.classList.add('grid-item');
-    divGrid.push(div);
 
-    divGrid.forEach(div => {
-        divContainer.appendChild(div);
-    })
+
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+
+    return "rgb(" + color.join(', ') + ")";
 }
+
+
+
+
+function gridCreation(number) {
+    for (let i = 0; i < number; i++) {
+        const div = document.createElement("div");
+        div.classList.add('grid-item');
+        divGrid.push(div);
+        divGrid.forEach(div => {
+            divContainer.appendChild(div);
+        })
+        
+        document.querySelectorAll(".grid-item").forEach(item => {
+            let bright =1;
+            item.addEventListener("mouseover", () => {
+                item.style.backgroundColor = randomColor();
+                bright -= 0.1;
+                bright = Math.max(bright, 0);
+                item.style.filter = `brightness(${bright})`;
+                
+            });
+            item.addEventListener("mouseleave", () => {
+                item.style.backgroundColor = "white";
+                
+            })
+        });
+    }
 }
 
-
-
-function gridRemotion(){
+function gridRemotion() {
     document.querySelectorAll(".grid-item").forEach(div => div.remove());
     divGrid = [];
 }
 
 
+
 const btn = document.querySelector(".btn");
-btn.addEventListener("click", ()=>{
+btn.addEventListener("click", () => {
     let gridSize = parseInt(prompt("Number of square per side of the new grid?"));
     gridRemotion();
     gridCreation(gridSize);
