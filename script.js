3f1d98c8274cb1a2845ea1ced9a88bf7adcffe1a
LuKa44x <lucavicari44@gmail.com>
2025-03-30 16:52:14 +0200

    Created function "createGrid"
    Created a function with all the style to make the grid.

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const container = document.querySelector('.container');
-const button = document.querySelector('.button');
+const button = document.querySelector('.reset');
+const grid = document.createElement('div');
+grid.classList.add('grid');
+container.appendChild(grid);
+
+createGrid(16);
+
+function createGrid(number){
+    for(let i = 0; i < number; i++){
+        const column = document.createElement('div');
+        column.classList.add('column');
+        grid.appendChild(column);
+        for(let j = 0; j < number; j++){
+            const square = document.createElement('div');
+            square.classList.add('square');
+            column.appendChild(square);
+        }
+    }
+}
